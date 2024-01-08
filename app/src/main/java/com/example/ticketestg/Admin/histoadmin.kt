7f5be6349da8a2792c1cfb3c244a62e6ee9f90fc
RubenR0drigues <rubenmrodrigues2@gmail.com>
2024-01-08 16:41:04 +0000

    criação da pagina e algum desgin ticket e historico admin

---
+++ app/src/main/java/com/example/ticketestg/Admin/histoadmin.kt
@@ -0,0 +1,12 @@
+package com.example.ticketestg.Admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ticketestg.R
+
+class histoadmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_histoadmin)
+    }
+}
